e15be2edf238c1a940ba859efabea5c284b59c3f
mehmet ali <202803055@ogr.cbu.edu.tr>
2021-03-07 13:32:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+choose=int(input("kac köşeli olsun"))
+import turtle as tr
+def Köse():
+    ok = tr.Turtle()
+    var = (choose - 2) * 180 / choose
+    for i in range(choose):
+        ok.forward(100)
+        ok.left(180-var)
+Köse()
+
+
+
+
